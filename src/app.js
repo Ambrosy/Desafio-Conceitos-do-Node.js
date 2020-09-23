e7b648a2eade54ae16c9401ca1c7ee747f1b1bb9
Rafael Souza Ambrosi <leafar2010@gmail.com>
2020-09-23 19:24:08 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -11,9 +11,8 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  const list = request.query;
-  list = [];
-  return response.json(list);
+
+  return response.json(repositories);
 
 });
 
@@ -29,7 +28,27 @@ app.post("/repositories", (request, response) => {
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const {title,url,techs} =request.body;
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+    if (repositoryIndex<0){
+        return response.status(400).json({
+            error: 'Repository not found.'
+        })
+    }
+    
+    const repository = {
+        id,
+        title,
+        owner,
+    };
+
+    repositories[repositoryIndex] = repository
+
+    return response.json(repository);
+
 });
 
 app.delete("/repositories/:id", (request, response) => {
